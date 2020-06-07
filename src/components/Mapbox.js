fd1cdeda3a941a264b7d9dfb6f06f5735455b683
Brian Bower <brian@brianbower.com>
2020-06-07 12:17:58 -0600

    Add mapbox component

---
+++ src/components/Mapbox.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
+require('dotenv').config();
+
+mapboxgl.accessToken = `${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`;
+
+class Mapbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1
+    };
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+  loadData() {
+  
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+
+    axios.get(`http://api.open-notify.org/iss-now.json`)
+      .then(res => {
+        const lat = res.data.iss_position.latitude
+        const long = res.data.iss_position.longitude
+
+        var el = document.createElement('div');
+        el.className = 'marker';
+
+        new mapboxgl.Marker(el)
+          .setLngLat([long, lat])
+          .addTo(map)
+
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(`<h3> International Space Station (ISS) </h3><p> Latitude: ${lat} </p><p> Longitude: ${long} </p>`))
+          .addTo(map);
+      });
+  }
+  
+
+  render() {
+
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} className="mapContainer" />
+      </div>
+    )
+  }
+}
+
+export default Mapbox
