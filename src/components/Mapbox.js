fca287652cc2e8922fa6f165b13a124bf3db3d3c
Brian Bower <brian@brianbower.com>
2020-06-07 13:54:41 -0600

    Add logic to update iss marker based on location stored in state

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -11,16 +11,17 @@ class Mapbox extends Component {
     this.state = {
       lng: 5,
       lat: 34,
-      zoom: 1
+      zoom: 1,
+      issLat: null,
+      issLong: null
     };
   }
 
   componentDidMount() {
-    this.loadData()
+    this.loadMap();
   }
 
-  loadData() {
-  
+  loadMap() {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -28,25 +29,35 @@ class Mapbox extends Component {
       zoom: this.state.zoom
     });
 
+    this.loadData(map)
+
+  }
+
+  loadData(map) {
+
     axios.get(`http://api.open-notify.org/iss-now.json`)
       .then(res => {
         const lat = res.data.iss_position.latitude
         const long = res.data.iss_position.longitude
+        this.setState({
+          issLat: lat,
+          issLong: long
+        })
 
         var el = document.createElement('div');
         el.className = 'marker';
 
         new mapboxgl.Marker(el)
-          .setLngLat([long, lat])
+          .setLngLat([this.state.issLong, this.state.issLat])
           .addTo(map)
 
         .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML(`<h3> International Space Station (ISS) </h3><p> Latitude: ${lat} </p><p> Longitude: ${long} </p>`))
+          .setHTML(`<h3> International Space Station (ISS) </h3><p> Latitude: ${this.state.issLat} </p><p> Longitude: ${this.state.issLong} </p>`))
           .addTo(map);
-      });
-  }
-  
 
+      })
+    }
+  
   render() {
 
     return (
