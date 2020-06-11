6f3bcc192e0adbccdfbd8bf656d63f67b2383764
Brian Bower <brian@brianbower.com>
2020-06-11 16:29:42 -0600

    Add instructions popup that disappears on click and update import to use SCSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Mapbox from './components/Mapbox'
+import InstructionPopup from './components/InstructionPopup';
 
 function App() {
+  const [showInstructions, setShowInstructions] = React.useState(false);
+  const onClick = () => setShowInstructions(true);
+
   return (
     <div>
+      <div onClick={onClick}>
+        {showInstructions ? null : <InstructionPopup />}
+      </div>
       <Mapbox />
     </div>
   );
