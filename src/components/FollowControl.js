db987fd364f45538c8023eebf7622f0c4714bffc
Brian Bower <brian@brianbower.com>
2020-06-12 17:08:05 -0600

    Add follow control to display the Follow and Stop Following buttons

---
+++ src/components/FollowControl.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import FollowButton from './FollowButton'
+import StopFollowingButton from './StopFollowingButton'
+
+class FollowControl extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFollowClick = this.handleFollowClick.bind(this)
+    this.handleStopFollowingClick = this.handleStopFollowingClick.bind(this)
+    this.state = {
+      isFollowing: false
+    }
+  }
+
+  handleFollowClick() {
+    this.setState({isFollowing: true})
+  }
+
+  handleStopFollowingClick() {
+    this.setState({isFollowing: false})
+  }
+
+  render() {
+    const isFollowing = this.state.isFollowing;
+    let button;
+    if (isFollowing) {
+      button = <StopFollowingButton onClick={this.handleStopFollowingClick} />;
+    } else {
+      button = <FollowButton onClick={this.handleFollowClick} />;
+    }
+
+    return (
+      <div>
+        {button}
+      </div>
+    )
+  }
+}
+
+export default FollowControl
