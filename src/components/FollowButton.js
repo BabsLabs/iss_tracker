dba30b5ac80bdd9fb25391e6fd0ea7b1cb0aa4ff
Brian Bower <brian@brianbower.com>
2020-06-17 10:58:46 -0600

    Update classnames to use new button class

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-
 export default function FollowButton(props) {
   return (
-    <button onClick={props.onClick} className='button-follow not-following'>
+    <button onClick={props.onClick} className='button follow not-toggled'>
       Follow
     </button>
   )
--- src/components/StopFollowingButton.js
+++ src/components/StopFollowingButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function StopFollowingButton(props) {
   return (
-    <button onClick={props.onClick} className='button-follow following'>
+    <button onClick={props.onClick} className='button follow toggled'>
       Stop Following
     </button>
   )
