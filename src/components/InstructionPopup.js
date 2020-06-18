25137e850b4691b1526b78b884d4b76082c4c1cb
Brian Bower <brian@brianbower.com>
2020-06-18 09:22:48 -0600

    Reduce text to make instructions simpler and add event marker

--- src/components/InstructionPopup.js
+++ src/components/InstructionPopup.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import NegativeIssLogo from '../images/ISS.png';
 import observatoryLogo from '../images/observatory.png';
+import eventLogo from '../images/event.png';
 
 class InstructionPopup extends Component {
   render() {
     return (
       <div id="instructions">
         <h1>International Space Station (ISS) Tracker</h1>
-        <p>- Click the ISS marker <img src={NegativeIssLogo} alt="(ISS LOGO)" className="iss-marker" /> for more info</p>
-        <p>- The position of the ISS is updated every 3 seconds</p>
-        <p>- Toggle all NASA registered ground based observatories by clicking the Observatory Toggle button<img src={observatoryLogo} alt="(ISS LOGO)" className="observatory-toggle-instructions" /></p>
-        <p>- Click an Observatory marker <img src={observatoryLogo} alt="(Observatory Marker)" className="observatory-marker" /> for more info</p>
+        <p>Toggle markers with the Observatory and Event buttons<img src={observatoryLogo} alt="Observatory Toggle" className="toggle-instructions" /><img src={eventLogo} alt="Event Toggle" className="toggle-instructions" /></p>
+        <p>Click markers <img src={NegativeIssLogo} alt="(ISS LOGO)" className="iss-marker marker" /> <img src={observatoryLogo} alt="Observatory Marker" className="observatory-marker marker" /> <img src={eventLogo} alt="Event Marker" className="event-marker marker" /> for more info</p>
+        <p>The position of the ISS is updated every 3 seconds</p>
         <h2>(Click to Proceed)</h2>
       </div>
     )
