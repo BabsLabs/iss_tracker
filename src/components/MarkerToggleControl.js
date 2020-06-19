21e943b37e1f22a67a03485bcd39d54da1881732
Brian Bower <brian@brianbower.com>
2020-06-19 12:57:55 -0600

    Add MapToggleControl component to render map style toggle button options

--- src/components/MarkerToggleControl.js
+++ src/components/MarkerToggleControl.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import ShowMarkerButton from './ShowMarkerButton';
 import HideMarkerButton from './HideMarkerButton';
+import MapButton from './MapStyleButton';
 import eventsLogo from '../images/event.png'
 import observatoryLogo from '../images/observatory.png'
+import streetsLogo from '../images/streets.jpg'
+import lightLogo from '../images/light.jpg'
+import darkLogo from '../images/dark.jpg'
+import satelliteLogo from '../images/satellite.jpg'
 
 class MarkerToggleControl extends Component {
   constructor(props) {
     super(props);
     this.handleShowClick = this.handleShowClick.bind(this)
     this.handleHideClick = this.handleHideClick.bind(this)
+    this.handleMapClick = this.handleMapClick.bind(this)
     this.state = {
-      isShowing: false
+      isShowing: false,
+      index: 0
     }
   }
 
@@ -21,6 +28,16 @@ class MarkerToggleControl extends Component {
 
   handleHideClick() {
     this.setState({ isShowing: false })
+  }
+
+  handleMapClick() {
+    let currentIndex = this.state.index
+    if (currentIndex < 3) {
+      let newIndex = this.state.index + 1;
+      this.setState({ index: newIndex })
+    } else (
+      this.setState({ index: 0 })
+    )
   }
 
   render() {
@@ -41,6 +58,21 @@ class MarkerToggleControl extends Component {
         button = <HideMarkerButton image={observatoryLogo} name={"observatory"} onClick={this.handleHideClick} />;
       } else {
         button = <ShowMarkerButton image={observatoryLogo} name={"observatory"} onClick={this.handleShowClick} />;
+      }
+    }
+
+    if (buttonName === "Map") {
+      switch (this.state.index) {
+        case 0:
+          return button = <HideMarkerButton image={lightLogo} name={"map"} onClick={this.handleMapClick} />;
+        case 1:
+          return button = <HideMarkerButton image={darkLogo} name={"map"} onClick={this.handleMapClick} />;
+        case 2:
+          return button = <HideMarkerButton image={satelliteLogo} name={"map"} onClick={this.handleMapClick} />;
+        case 3:
+          return button = <HideMarkerButton image={streetsLogo} name={"map"} onClick={this.handleMapClick} />;
+        default:
+          return button = <MapButton image={streetsLogo} name={"map"} onClick={this.handleMapClick} />;
       }
     }
 
